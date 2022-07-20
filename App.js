c95e1c6bb5298fb2a97e154e36d7c1cb602733b9
Don Romaniello <don.romaniello@gmail.com>
2022-07-20 12:05:18 -0400

    Add stylesheet.

--- App.js
+++ App.js
@@ -1,19 +1,33 @@
-import { View } from "react-native";
+import {
+  Text,
+  View,
+  StyleSheet
+ } from "react-native";
 
-// From https://simpleicons.org/?q=expo
 import ExpoLogo from "./assets/expo.svg";
 
 export default function App() {
   return (
     <View
-      style={{
-        backgroundColor: "black",
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+      style={styles.container}
     >
-      <ExpoLogo width={120} height={120} fill="white" />
+      <Text
+      style={styles.header}>
+        It works.
+      </Text>
+      {/* <ExpoLogo width={120} height={120} fill="white" /> */}
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "blue",
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  header : {
+    fontSize: 64
+  }
+});
